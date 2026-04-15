310b3174c62cab968d1859f169b59640ea88c2c6
Hanshika Mukati <hanshikamukati10@gmail.com>
2026-04-15 12:44:40 +0530

    Update login.java

--- src/model/login.java
+++ src/model/login.java
@@ -1,29 +1,51 @@
-package model;
+package model; // Model package (stores only data classes)
 
+// This class represents login credentials of a user
 public class login {
-    private String email;
-    private String password;
 
-    public login() {}
+    // ==============================
+    // FIELDS (DATA MEMBERS)
+    // ==============================
 
+    private String email; // stores user email
+    private String password; // stores user password
+
+    // ==============================
+    // DEFAULT CONSTRUCTOR
+    // ==============================
+    public login() {} 
+    // empty constructor (used when object is created without values)
+
+    // ==============================
+    // PARAMETERIZED CONSTRUCTOR
+    // ==============================
     public login(String email, String password) {
-        this.email = email;
-        this.password = password;
+
+        this.email = email; // assign email
+        this.password = password; // assign password
     }
+
+    // ==============================
+    // GETTERS (READ DATA)
+    // ==============================
 
     public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        return email; // return email
     }
 
     public String getPassword() {
-        return password;
+        return password; // return password
+    }
+
+    // ==============================
+    // SETTERS (UPDATE DATA)
+    // ==============================
+
+    public void setEmail(String email) {
+        this.email = email; // update email
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = password; // update password
     }
 }
